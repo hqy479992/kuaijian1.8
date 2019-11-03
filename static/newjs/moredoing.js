2f2d4bb426af4251907f1c43324ca9cfbe098b26
hqy479992 <1430618977@qq.com>
2019-11-03 14:10:23 +0800

    解决bug

--- static/newjs/moredoing.js
+++ static/newjs/moredoing.js
@@ -1,13 +1,11 @@
-//全局变量
 var task_list = [];
-var bari;
-
-window.onload = function () {
-
-	getProgress()
-    setInterval(function () { moreTask() }, 5000)
-}
-
+$(function () {
+	getProgress();
+	createList(task_list)
+	createBar(task_list)
+	setInterval(function () { moreTask() },2000)
+})
+//请求到数据
 function getProgress() {
 	var obj = {
 		'task_name': 'task_name',
@@ -17,47 +15,73 @@ function getProgress() {
 		url: '/progress',
 		type: 'POST',
 		data: JSON.stringify(obj),
-		async: true,
+		async: false,
 		success: function (data) {
 			if (data == 'error') {
 				//error
 			} else {
-				data = JSON.parse(data)
-				moreTask(data)
+				task_list = JSON.parse(data)
+			}
+		}
 
+	})
+	return task_list;
+}
+
+//动态创建列表
+function createList(data) {
+	$.each(data, function (index, item) {
+		$(".bar").append(`
+				<div class="row bar-list bar-list${index}">
+					<div class="col-2">
+						<span>${item.task_name}</span>
+					</div>
+					<div class="col-1">
+						<span class="fa fa-times-circle task"  onclick="closeBar(event,\'${item.task_name}\')"></span>
+					</div>
+				</div>
+			`)
+	})
+}
+// 进度条列表
+function createBar(data) {
+	$.each(data, function (index, item) {
+	    var rate = item.progress_rate *100
+	    rate  = rate.toFixed(3);
+		$(".bar-list" + index).append(`
+		<div class="progress  col-9 mt-2 " style = "padding:0px;">
+		<div class="progress-bar progress-bar-striped progress-bar-animated bg-info"  role="progressbar" aria-valuemin="0" aria-valuemax="100"  style="width: ${rate}%;">
+		    <span>${rate}%</span>
+		</div>
+	</div>
+		`)
+	})
+}
+function moreTask(){
+	var obj = {
+		'task_name': 'task_name',
+		'progress_rate': 'progress_rate'
+	}
+	$.ajax({
+		url: '/progress',
+		type: 'POST',
+		data: JSON.stringify(obj),
+		async: false,
+		success: function (data) {
+			if (data == 'error') {
+				//error
+			} else {
+				$.each(JSON.parse(data),function(index,item){
+				   var rate = item.progress_rate *100
+	                rate  = rate.toFixed(3);
+					$(".bar-list" + index + " .progress-bar").css("width",rate+"%");
+					$(".bar-list" + index + " .progress-bar span").text(rate+"%");
+				})
 			}
 		}
 
 	})
 }
-
-function moreTask(data){
-    $.each(data, function (i, item) {
-					console.log(item.task_name)
-					console.log(item.progress_rate)
-					$('.bar').append(`
-                             	<div class="row bar-list">
-                                    <div class="col-2">
-                                        <span>${item.task_name}</span>
-                                    </div>
-                                    <div class="progress  col-9 mt-2 " style = "padding:0px;">
-                                        <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" id="bar${i}" role="progressbar" aria-valuemin="0" aria-valuemax="100" style=""><span></span></div>
-                                    </div>
-                                    <div class="col-1">
-                                        <span class="fa fa-times-circle task"  onclick="closeBar(event,\'${item.task_name}\')"></span>
-                                    </div>
-			                    </div>`)
-
-								var rate = item.progress_rate * 100
-								rate = rate.toFixed(2)
-								bari = 'bar'+i.toString()
-								$('div#'+bari).css('width', rate + '%')
-								$('div#'+bari+' span').text(rate + '%')
-
-				})
-}
-
-//关闭
 function closeBar(e, name) {
 	// name = name || "";
 	$(e.target).parent().parent().remove();
@@ -80,4 +104,3 @@ function closeBar(e, name) {
 		}
 	})
 }
-
