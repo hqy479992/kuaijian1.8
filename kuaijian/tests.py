56c08f7b7149692dce3aa2b4da9f89ebf1441542
hehaoyu <hehaoyulkeke@163.com>
2019-11-08 10:52:20 +0800

    limit running tasks less than 4

--- kuaijian/tests.py
+++ kuaijian/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+import time
+from kuaijian.util.executor import Executor
+from multiprocessing import Queue
 
-# Create your tests here.
+
+def f(i1, q1):
+    print(str(i1)+' start')
+    time.sleep(2)
+    q1.put(1)
+    print(str(i1)+'end')
+
+if __name__ == '__main__':
+    e = Executor()
+    for i in range(7):
+        q = Queue()
+        name = 'task-' + str(i)
+        e.submit(name, q, f, i, q)
+    for item in e.get_all_tasks():
+        print(item)
+        print(e.get_process(item))
+    time.sleep(3)
+    for item in e.get_all_tasks():
+        print(item)
+        print(e.get_process(item))
+    time.sleep(3)
+    for item in e.get_all_tasks():
+        print(item)
+        print(e.get_process(item))
--- kuaijian/util/executor.py
+++ kuaijian/util/executor.py
@@ -7,22 +7,38 @@ class Executor:
         self._tasks = {}
         self._queues = {}
         self._rate_cache = {}
+        self._buffer = []
 
     def submit(self, task_name, queue, func, *args):
-        p = Process(target=func, args=args)
-        self._tasks[task_name] = p
-        self._queues[task_name] = queue
-        self._rate_cache[task_name] = 0.01
-        p.start()
+        if len(self._tasks) >= 4:
+            print('{} add to cache'.format(task_name))
+            self._rate_cache[task_name] = 0.0
+            self._buffer.append((task_name, queue, func, args))
+        else:
+            p = Process(target=func, args=args)
+            self._tasks[task_name] = p
+            self._queues[task_name] = queue
+            self._rate_cache[task_name] = 0.01
+            print('start run task: {}'.format(task_name))
+            p.start()
 
     def stop(self, task_name):
         self._tasks[task_name].terminate()
-        del self._tasks[task_name]
-        del self._queues[task_name]
+        self._clean_infos(task_name)
         print("successful stopped task {}".format(task_name))
+        if len(self._buffer) > 0:
+            task_name, queue, func, args = self._buffer.pop(0)
+            self.submit(task_name, queue, func, *args)
 
     def get_all_tasks(self):
-        return list(self._tasks.keys())
+        res = list(self._tasks.keys())
+        res.extend([task[0] for task in self._buffer])
+        return res
+
+    def _clean_infos(self, task_name):
+        del self._tasks[task_name]
+        del self._queues[task_name]
+        del self._rate_cache[task_name]
 
     def get_process(self, task_name):
         try:
@@ -30,4 +46,8 @@ class Executor:
                 rate = self._queues[task_name].get_nowait()
                 self._rate_cache[task_name] = rate
         except:
+            if len(self._buffer) > 0 and self._rate_cache[task_name] >= 1:
+                self._clean_infos(task_name)
+                task_name, queue, func, args = self._buffer.pop(0)
+                self.submit(task_name, queue, func, *args)
             return self._rate_cache[task_name]
